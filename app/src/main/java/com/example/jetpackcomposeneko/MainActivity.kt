75d21d1e13d4cb1478f6dc25fcdab3c0110d0cd8
Lev <blacktouring@yandex.ru>
2022-07-28 02:46:19 +0300

    add ListItem

--- app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
@@ -3,47 +3,62 @@ package com.example.jetpackcomposeneko
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier
-                    .background(Color.Gray)
-                    .fillMaxSize(),
-                horizontalArrangement = Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column(
-                    modifier = Modifier
-                        .background(Color.Green)
-                        .fillMaxWidth(0.5f).fillMaxHeight(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "12345")
-                    Text(text = "23456")
-                    Text(text = "34567")
-                }
-                Column(
-                    modifier = Modifier
-                        .background(Color.Magenta)
-                        .fillMaxSize(),
-                    verticalArrangement = Arrangement.SpaceAround,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(text = "12345")
-                    Text(text = "23456")
-                    Text(text = "34567")
+            Column() {
+                ListItem("Kurt Cobain", "RockStar")
+                ListItem("Kurt Cobain", "RockStar")
+                ListItem("Kurt Cobain", "RockStar")
+                ListItem("Kurt Cobain", "RockStar")
+                ListItem("Kurt Cobain", "RockStar")
+                ListItem("Kurt Cobain", "RockStar")
+            }
+        }
+    }
+
+    @Composable
+    private fun ListItem(name: String, profession: String) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp
+        ) {
+            Box() {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = painterResource(id = R.drawable.kurt),
+                        contentDescription = "photo",
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .size(64.dp)
+                            .clip(CircleShape)
+                    )
+                    Column() {
+                        Text(text = name)
+                        Text(text = profession)
+                    }
                 }
             }
         }
     }
 }
+
