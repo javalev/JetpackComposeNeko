82fd6ff345c7d5ebf196498651c59dd3abf03a5d
Lev <blacktouring@yandex.ru>
2022-07-27 23:56:14 +0300

    add NewText

--- app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
@@ -3,12 +3,21 @@ package com.example.jetpackcomposeneko
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            NewText()
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NewText(){
+    Text(text = "AVRIL LAVIGNE SUPERSTAR!!")
 }
