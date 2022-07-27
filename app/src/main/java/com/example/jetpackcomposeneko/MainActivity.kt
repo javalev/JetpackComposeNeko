e5837576240838fd9ea03e9bc176e2d38125a49b
Lev <blacktouring@yandex.ru>
2022-07-28 00:58:49 +0300

    add row and column

--- app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
@@ -3,21 +3,47 @@ package com.example.jetpackcomposeneko
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            Row(
+                modifier = Modifier
+                    .background(Color.Gray)
+                    .fillMaxSize(),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Column(
+                    modifier = Modifier
+                        .background(Color.Green)
+                        .fillMaxWidth(0.5f).fillMaxHeight(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "12345")
+                    Text(text = "23456")
+                    Text(text = "34567")
+                }
+                Column(
+                    modifier = Modifier
+                        .background(Color.Magenta)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.SpaceAround,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = "12345")
+                    Text(text = "23456")
+                    Text(text = "34567")
+                }
+            }
         }
     }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun NewText(){
-    Text(text = "AVRIL LAVIGNE SUPERSTAR!!")
 }
