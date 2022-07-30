82aaa3f486d3526bd6ee9d16f4cbd560f97abbe9
Lev <blacktouring@yandex.ru>
2022-07-31 02:30:28 +0300

    add circle item with counter and backColor state

--- app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeneko/MainActivity.kt
@@ -1,85 +1,61 @@
 package com.example.jetpackcomposeneko
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectHorizontalDragGestures
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-                ListItem("Kurt Cobain1", "RockStar")
-                ListItem("Kurt Cobain2", "RockStar")
-                ListItem("Kurt Cobain3", "RockStar")
-                ListItem("Kurt Cobain4", "RockStar")
-                ListItem("Kurt Cobain5", "RockStar")
-                ListItem("Kurt Cobain6", "RockStar")
-                ListItem("Kurt Cobain7", "RockStar")
-                ListItem("Kurt Cobain8", "RockStar")
-                ListItem("Kurt Cobain9", "RockStar")
-                ListItem("Kurt Cobain10", "RockStar")
-                ListItem("Kurt Cobain11", "RockStar")
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircleItem()
             }
         }
     }
 
     @Composable
-    private fun ListItem(name: String, profession: String) {
-        Card(
+    private fun CircleItem() {
+        var counter = remember {
+            mutableStateOf(0)
+        }
+        var color = remember {
+            mutableStateOf(Color.Green)
+        }
+        Box(
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp)
-                .offset(10.dp, 10.dp)
-                .clickable { Log.d("mytag", "click card + $name") }
-                .pointerInput(Unit) {
-                    detectHorizontalDragGestures { change, dragAmount ->
-                        Log.d(
-                            "mytag",
-                            "click card + $dragAmount"
-                        )
+                .size(150.dp)
+                .background(color.value, shape = CircleShape)
+                .clickable {
+                    when (++counter.value) {
+                        10 -> color.value = Color.Red
+                        20 -> color.value = Color.Black
                     }
-                },
-            shape = RoundedCornerShape(15.dp),
-
-            elevation = 5.dp
+                }, contentAlignment = Alignment.Center
         ) {
-            Box() {
-                Row(verticalAlignment = Alignment.CenterVertically) {
-                    Image(
-                        painter = painterResource(id = R.drawable.kurt),
-                        contentDescription = "photo",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .padding(8.dp)
-                            .size(64.dp)
-                            .clip(CircleShape)
-                    )
-                    Column() {
-                        Text(text = name)
-                        Text(text = profession)
-                    }
-                }
-            }
+            Text(
+                text = counter.value.toString(),
+                style = TextStyle(color = Color.White, fontSize = 35.sp)
+            )
         }
     }
 }
